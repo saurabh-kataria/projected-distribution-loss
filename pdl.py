0a1292752c3e2c45172754b49f5b03d1ba8ac161
Saurabh <saurabh-kataria@users.noreply.github.com>
2020-12-22 16:00:53 -0500

    Update pdl.py

--- pdl.py
+++ pdl.py
@@ -16,27 +16,6 @@ def projectedDistributionLoss(x, y, num_projections=1000):
         projections = torch.randn((dim,num_projections), device=device)
         projections = projections / torch.sqrt(torch.sum(projections ** 2, dim=0, keepdim=True))    # columns are unit length normalized
         return projections
-    def reduce(x, reduction="mean"):
-        """Batch reduction of a tensor."""
-        if reduction == "sum":
-            x = x.sum()
-        elif reduction == "mean":
-            x = x.mean()
-        elif reduction == "none":
-            x = x
-        else:
-            raise ValueError("unkown reduction={}.".format(reduction))
-        return x
-    def huber_loss(pred, target, delta=1e-3, reduction="mean"):
-        """Computes the Huber loss."""
-        diff = pred - target
-        abs_diff = torch.abs(diff)
-        with amp.autocast(enabled=False):
-            loss = torch.where(abs_diff < delta,
-                               0.5 * (diff+EPS)**2,
-                               delta * (abs_diff - 0.5 * delta))
-        loss = reduce(loss, reduction=reduction)
-        return loss
     x = x.reshape(x.shape[0], x.shape[1], -1)   # B,N,M
     y = y.reshape(y.shape[0], y.shape[1], -1)
     W = rand_projections(x.shape[-1], device=x.device, num_projections=num_projections)#x.shape[-1])
